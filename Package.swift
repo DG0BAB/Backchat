784aa28927837f88978099e21f22aba6f25f2e11
Joachim Deelen <jdeelen@micabo.de>
2020-05-29 15:27:28 +0200

    Switched to local dependencies

--- Package.swift
+++ Package.swift
@@ -11,9 +11,15 @@ let package = Package(name: packageName)
 
 // Products define the executables and libraries produced by a package, and make them visible to other packages.
 package.products = [.library(name: packageName, targets: [packageName])]
-package.platforms = [.iOS(.v13)]
-package.dependencies = [.package(url: "https://github.com/mxcl/PromiseKit", from: "6.10.0")]
+package.platforms = [.iOS(.v13), .macOS(.v10_15)]
+package.dependencies = [
+	.package(url: "https://github.com/mxcl/PromiseKit", from: "6.10.0"),
+//	.package(url: "git@github.com:DG0BAB/Clause.git", .path("develop")),
+	.package(name: "Clause", path: "../../Clause/ClausePackage"),
+//	.package(url: "git@github.com:DG0BAB/Fehlerteufel.git", .branch("develop")),
+	.package(name: "Fehlerteufel", path: "../../Fehlerteufel/FTPackage"),
+]
 package.targets = [
-	.target(name: packageName, dependencies: ["PromiseKit"], path: "Sources"),
+	.target(name: packageName, dependencies: ["Clause", "Fehlerteufel", "PromiseKit"], path: "Sources"),
 	.testTarget(name: "\(packageName)Tests", dependencies: [Target.Dependency(stringLiteral: packageName)]),
 ]
