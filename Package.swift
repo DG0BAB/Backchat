bd4424232d1988e51f2f028d78b521e53d41b26d
Joachim Deelen <jdeelen@micabo.de>
2020-06-07 19:43:17 +0200

    Added dependency for the PetiteLogger Updated structure

--- Package.swift
+++ Package.swift
@@ -6,20 +6,39 @@ import PackageDescription
 // Name of this Package
 let packageName = "backchat"
 
-// Package creation
 let package = Package(name: packageName)
 
-// Products define the executables and libraries produced by a package, and make them visible to other packages.
-package.products = [.library(name: packageName, targets: [packageName])]
-package.platforms = [.iOS(.v13), .macOS(.v10_15)]
+package.products = [
+	.library(name: packageName,
+			 targets: [packageName])
+]
+
+package.platforms = [
+	.iOS(.v13),
+	.macOS(.v10_15)
+]
+
 package.dependencies = [
 	.package(url: "https://github.com/mxcl/PromiseKit", from: "6.10.0"),
 //	.package(url: "git@github.com:DG0BAB/Clause.git", .path("develop")),
+	.package(name: "PetiteLogger", path: "../../PetiteLogger/PLPackage"),
 	.package(name: "Clause", path: "../../Clause/ClausePackage"),
 //	.package(url: "git@github.com:DG0BAB/Fehlerteufel.git", .branch("develop")),
 	.package(name: "Fehlerteufel", path: "../../Fehlerteufel/FTPackage"),
 ]
+
+let targetDependencies: [Target.Dependency] = [
+	"PetiteLogger",
+	"Clause",
+	"Fehlerteufel",
+	"PromiseKit"
+]
+
 package.targets = [
-	.target(name: packageName, dependencies: ["Clause", "Fehlerteufel", "PromiseKit"], path: "Sources"),
-	.testTarget(name: "\(packageName)Tests", dependencies: [Target.Dependency(stringLiteral: packageName)]),
+	.target(name: packageName,
+			dependencies: targetDependencies,
+			path: "Sources"),
+
+	.testTarget(name: "\(packageName)Tests",
+		dependencies: [Target.Dependency(stringLiteral: packageName)])
 ]
